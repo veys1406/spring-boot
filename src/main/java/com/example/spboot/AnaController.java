5176d31564bd5234762d32853b4a49c2e337a980
Veyselkarani Ülkü <veyselkarani.ulku@std.ankaramedipol.edu.tr>
2026-08-09 15:36:52 +0300

    add path variable endpoint

--- src/main/java/com/example/spboot/AnaController.java
+++ src/main/java/com/example/spboot/AnaController.java
@@ -1,6 +1,7 @@
 package com.example.spboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,9 @@ public class AnaController {
     @GetMapping("/")
     public String returnString() {
         return "First message.";
+    }
+    @GetMapping("/merhaba/{name}")
+    public String returnName(@PathVariable String name ) {
+        return "Selam " + name;
     }
 }
