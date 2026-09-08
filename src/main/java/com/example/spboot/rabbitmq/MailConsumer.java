909f3d44f53f0cee86cc687f3c5a7a24a5dbeb99
Veyselkarani Ülkü <veyselkarani.ulku@std.ankaramedipol.edu.tr>
2026-09-08 16:53:10 +0300

    send welcome mail from MailConsumer via JavaMailSender

--- src/main/java/com/example/spboot/rabbitmq/MailConsumer.java
+++ src/main/java/com/example/spboot/rabbitmq/MailConsumer.java
@@ -1,16 +1,34 @@
 package com.example.spboot.rabbitmq;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import com.example.spboot.dto.MailUsername;
 
 @Component 
 public class MailConsumer {
+    private final JavaMailSender javaMailSender;
     
+    public MailConsumer(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
     @RabbitListener(queues = "mail-queue")
     public void print(MailUsername mailUsername){
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setTo(mailUsername.getUserMail());
+        simpleMailMessage.setFrom("noreply@spboot.local");
+        simpleMailMessage.setSubject("Hosgeldin!");
+        simpleMailMessage.setText("Aramiza hosgeldin " + mailUsername.getUsername() + ".");
+        
+        javaMailSender.send(simpleMailMessage);
+
         System.out.println(mailUsername.getUserMail() +" "+ mailUsername.getUsername());
+
     }
 
 }
