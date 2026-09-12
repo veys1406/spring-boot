47ab4be4ddb1132cef7918b618dd25671760d65a
Veyselkarani Ülkü <veyselkarani.ulku@std.ankaramedipol.edu.tr>
2026-09-12 15:03:57 +0300

    skip already processed messages using a Redis key

--- src/main/java/com/example/spboot/rabbitmq/MailConsumer.java
+++ src/main/java/com/example/spboot/rabbitmq/MailConsumer.java
@@ -1,8 +1,11 @@
 package com.example.spboot.rabbitmq;
 
+import java.time.Duration;
+
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.messaging.handler.annotation.Header;
@@ -13,13 +16,21 @@ import com.example.spboot.dto.MailUsername;
 @Component 
 public class MailConsumer {
     private final JavaMailSender javaMailSender;
+    private final RedisTemplate<Object, Object> redisTemplate;
     
-    public MailConsumer(JavaMailSender javaMailSender) {
+    public MailConsumer(JavaMailSender javaMailSender, RedisTemplate<Object, Object> redisTemplate) {
         this.javaMailSender = javaMailSender;
+        this.redisTemplate = redisTemplate;
     }
 
     @RabbitListener(queues = "mail-queue")
     public void print(MailUsername mailUsername, @Header(AmqpHeaders.MESSAGE_ID) String message_id){// exception atarsa nack atmazsa ack
+
+        if(redisTemplate.hasKey(message_id)){// nulla karsi korumuyor
+            System.out.println("Bu mesaj zaten gonderildi -> "+ message_id);
+            return;
+        }
+
 
         if( (mailUsername.getUserMail()==null) || !(mailUsername.getUserMail().contains("@"))){
             throw new AmqpRejectAndDontRequeueException("Gecersiz mail!");
@@ -32,7 +43,12 @@ public class MailConsumer {
         simpleMailMessage.setSubject("Hosgeldin!");
         simpleMailMessage.setText("Aramiza hosgeldin " + mailUsername.getUsername() + ".");
         
+        //at-least-once
         javaMailSender.send(simpleMailMessage);// exception firlatabilir
+        redisTemplate.opsForValue().set(message_id, "mailSent", Duration.ofDays(1));
+        
+        
+
         System.out.println(message_id);
         try {
             Thread.sleep(10000);
